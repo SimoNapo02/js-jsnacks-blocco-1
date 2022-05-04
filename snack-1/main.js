d370e19b36691cd9269dc512b3bdc4d5d5611641
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-05-04 11:33:59 +0200

    inserisci numero js

---
+++ snack-1/main.js
@@ -0,0 +1,3 @@
+// L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
+const firstNumber = Number(prompt("Inserisci il primo numero"));
+const secondNumber = Number(prompt("Inserisci il secondo numero"));
