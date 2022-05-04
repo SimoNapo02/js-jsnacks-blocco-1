6322f056d34143c36a8166ae17e282363c40f4ee
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-05-04 11:46:55 +0200

    js

---
+++ snack-2/main.js
@@ -0,0 +1,8 @@
+// Il software stampa prima la parola più corta, poi la parola più lunga
+
+// Chiedo all'user la prima parola
+const ParolaUno = Text(prompt("inserisci la prima parola"))
+// Chiedo all'user la seconda parola
+const ParolaDue = Text(prompt("inserisci la seconda parola"))
+
+
