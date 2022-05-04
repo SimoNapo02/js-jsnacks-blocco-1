efb4c904419ba328536e587b4efc2c7bdd8d7a49
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-05-04 12:19:06 +0200

    aggiunto somma

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,8 +1,11 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
-let numero = 0; 
 
-for (let i = 0; i < 10; i++) {
-    numero += Number(prompt('inserisci un numero'));    
+let somma = 0;
+
+for( let i=0 ; i < 10 ; i++){
+   let numero = Number(prompt("inserisci un numero"));
+   somma += numero;
 }
 
 //  Il programma stampa la somma di tutti i numeri inseriti.
+console.log("la somma è ", somma)
