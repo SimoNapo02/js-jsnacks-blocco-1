96316d9ebb6e74c0c1e3683689362daa772e5b3e
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-05-04 12:08:31 +0200

    chiedi numeri js

---
+++ snack-3/main.js
@@ -0,0 +1,8 @@
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+let numero = 0; 
+
+for (let i = 0; i < 10; i++) {
+    numero += Number(prompt('inserisci un numero'));    
+}
+
+//  Il programma stampa la somma di tutti i numeri inseriti.
