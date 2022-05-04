645310a353419d6e9ced9be0c3188bfc660bf3d4
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-05-04 11:36:33 +0200

    js

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,3 +1,12 @@
-// L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
-const firstNumber = Number(prompt("Inserisci il primo numero"));
-const secondNumber = Number(prompt("Inserisci il secondo numero"));
+// L’utente inserisce due numeri in successione, con due prompt.
+const NumeroUno = Number(prompt("Inserisci il primo numero"));
+const NumeroDue = Number(prompt("Inserisci il secondo numero"));
+
+// Il software stampa il maggiore.
+if (NumeroUno > NumeroDue) {
+    console.log(NumeroUno);
+} else if (NumeroUno < NumeroDue){
+    console.log(NumeroDue);
+} else {
+    console.log("i numeri sono gli stessi");
+}
